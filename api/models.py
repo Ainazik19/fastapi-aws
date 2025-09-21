1dec7d5d0f6c9e15cdf7e5d9987cdb81c6e32768
Ainazik Orunbaeva <ainazikorunbaeva@Ainazikui-MacBookAir.local>
2025-09-21 18:03:02 +0900

    Procfile

--- api/models.py
+++ api/models.py
@@ -9,10 +9,12 @@ load_dotenv()
 
 Base = declarative_base()
 
-if os.getenv("DEPLOYMENT_ENVIRONMENT") == 'DEV':
-    engine = create_engine(os.getenv("DB_URL"), connect_args={'check_same_thread': False})
-else:
-    engine = create_engine(os.getenv("DB_URL"))
+#if os.getenv("DEPLOYMENT_ENVIRONMENT") == 'DEV':
+#    engine = create_engine(os.getenv("DB_URL"), connect_args={'check_same_thread': False})
+#else:
+#    engine = create_engine(os.getenv("DB_URL"))
+
+engine = create_engine("postgresql://rootuser:databasepassword@fastapi-aws-database.c7m68scm8ba1.us-east-2.rds.amazonaws.com:5432/postgres")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
